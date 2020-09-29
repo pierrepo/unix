b44b7340d8dcade84bb652cb1a4223e3fcdcf694
Pierre Poulain <pierre.poulain@cupnet.net>
2020-09-29 17:58:56 +0200

    Add random sorted script

---
+++ get_staphylo_puzzle.sh
@@ -0,0 +1,16 @@
+orga=$(awk '/ORGANISM/{print $2}' ${name})
+done
+cp ${name} ${dir_output}
+do
+rm -rf ${dir_input} ${dir_output}
+for name in ${dir_input}/*.gbk
+fi
+dir_output="staphylo"
+tar zxf genomes.tgz
+echo "${name} is a Staphylococcus"
+tar zcf staphylo.tgz ${dir_output}/
+mkdir ${dir_output}
+then
+dir_input="genomes"
+#! /usr/bin/env bash
+if [[ ${orga} = "Staphylococcus" ]]
